b91d1fbbe64e9d68639d58bd3e0e5f2bb4a1092e
devwalker3 <vatik.vag@gmail.com>
2018-01-08 14:17:56 +0200

    Second Commit

--- src/main/java/com/tuc/thesis/spring/boot/web/map_app/spot/scuba_diving/diveSchoolSpot/DiveSchoolSpotRepository.java
+++ src/main/java/com/tuc/thesis/spring/boot/web/map_app/spot/scuba_diving/diveSchoolSpot/DiveSchoolSpotRepository.java
@@ -3,4 +3,3 @@ package com.tuc.thesis.spring.boot.web.map_app.spot.scuba_diving.diveSchoolSpot;
 import org.springframework.data.repository.CrudRepository;
 
 public interface DiveSchoolSpotRepository extends CrudRepository<DiveSchoolSpot, Integer> {}
-git
